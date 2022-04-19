aef98c60d79f03f99a5592c86b4ceb767441fa20
sake0619 <648685144@qq.com>
2022-04-19 23:49:52 +0800

    完成了主页功能

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+$(function () {
+    getUserInfo()
+    $('#btnLogout').on('click', function () {
+        layui.layer.confirm('确定退出登录', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'GET',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data)
+        },
+
+    })
+}
+function renderAvatar(user) {
+    let name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        $('.text-avatar').html(name[0].toUpperCase()).show()
+    }
+}
